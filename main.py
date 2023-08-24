fea058ef7ed5fc4e2d4bd3fe18c31dbcc4a62518
Arun Kumar Perumal <82801641+arper9@users.noreply.github.com>
2023-08-24 02:32:55 +0200

    Refactor main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-** 
+**
 
 # Initialize the purchase_amt_DSA_Self_Paced variable
 purchase_amt_DSA_Self_Paced = 10000
@@ -7,3 +7,5 @@ purchase_amt_DSA_Self_Paced = 10000
 # Purchase amount must be more than 2999 to be eligible
 if purchase_amt_DSA_Self_Paced > 2999:
     print("You are eligible to purchase DSA Self Paced")
+else:
+    print("Your purchase amount is not above 2999. You are not eligible to purchase DSA Self Paced.")
