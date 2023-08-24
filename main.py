94f71ecc122b20e97b502333ee2a54f82a620674
arunkumar-p8791-se <143039961+arunkumar-p8791-se@users.noreply.github.com>
2023-08-24 02:00:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-
-
-# Refactored start
+# initialize the amount variable
 amount = 10000
+  
+# check that You are eligible to
+#  purchase Dsa Self Paced or not
 
-if amount > 2999:
+if(amount>2999):
     print("You are eligible to purchase Dsa Self Paced")
-# Refactored end
+
+test-78
