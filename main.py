c8bc487c3f5f715956b33045306c20f13fb45014
Arun Kumar Perumal <82801641+arper9@users.noreply.github.com>
2023-08-24 02:26:16 +0200

    Refactor main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 
 
-# Initialize the purchase_total variable
-purchase_total = 10000
+# Initialize the purchase_amount variable
+purchase_amount = 10000
 
 # Check that you are eligible to purchase DSA Self Paced
-# Purchase total must be more than 2999 to be eligible
-if purchase_total > 2999:
+# Purchase amount must be more than 2999 to be eligible
+if purchase_amount > 2999:
     # Print out eligible message
     print("You are eligible to purchase DSA Self Paced")
