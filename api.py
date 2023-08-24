a2474ce84d93a563a0b6eef65735af7b85ad87be
Arun Kumar Perumal <82801641+arper9@users.noreply.github.com>
2023-08-24 02:26:39 +0200

    Refactor api.py

--- api.py
+++ api.py
@@ -9,10 +9,11 @@ app = Flask(__name__)
 @app.route('/hello', methods=['GET'])
 def helloworld():
     # Create a dictionary containing the response data
-    response_data = {'data': 'Hello World'}
+    response_data = {
+        'data': 'Hello World'
+    }
     # Return the response data in a JSON format
     return jsonify(response_data)
-  
   
 if __name__ == '__main__':
     app.run(debug=False)
