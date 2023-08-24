8e3d1fc41a949f2d34fada32910373c60e916e08
Arun Kumar Perumal <82801641+arper9@users.noreply.github.com>
2023-08-24 02:02:05 +0200

    Refactor main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ price_threshold = 2999
 # Check if the user is eligible to purchase Dsa Self Paced
 if(price_threshold < amount):
     print("You are eligible to purchase Dsa Self Paced")
+else:
+    print("The purchase amount is lower than the threshold")
     
 # Test value
 test_value = 78
